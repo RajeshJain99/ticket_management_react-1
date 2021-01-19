f3577d540c6b258686d1e764e3099810cb7f17bf
divysoft <rajeshahudr3@gmail.com>
2021-01-19 18:06:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { userContext } from './context/UserContext'
 import './scss/style.scss';
 import './App.css'
 import 'react-toastify/dist/ReactToastify.css';
@@ -19,23 +20,23 @@ const Register = React.lazy(() => import('./views/pages/register/Register'));
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 
-class App extends Component {
+function App() {
 
-  render() {
-    return (
-      <HashRouter>
-        <React.Suspense fallback={loading}>
-          <Switch>
-            <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
-            <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
-            <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
-            <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
-            <Route path="/" name="Home" render={props => <TheLayout {...props} />} />
-          </Switch>
-        </React.Suspense>
-      </HashRouter>
-    );
-  }
+  const { user } = React.useContext(userContext)
+  return (
+    <HashRouter>
+      <React.Suspense fallback={loading}>
+        <Switch>
+          <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
+          <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
+          <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
+          <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
+          {user ? <Route path="/" name="Home" render={props => <TheLayout {...props} />} /> : <Redirect to='/login' />}
+        </Switch>
+      </React.Suspense>
+    </HashRouter>
+  );
+  // }
 }
 
 export default App;
