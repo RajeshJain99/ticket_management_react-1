b7ad9809a8db86e0aa03d10848159646295e9a2a
RajeshJain99 <rajeshahudr3@gmail.com>
2021-01-21 16:23:34 +0530

    yahya

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -1,2 +1,2 @@
 // export const url = 'http://localhost:3002/';
-export const url = 'http://6999387c71a7.ngrok.io/';
+export const url = 'http://66521fe140a9.ngrok.io/';
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,27 +1,14 @@
 import React, { lazy } from 'react'
-import {
-  CBadge,
-  CButton,
-  CButtonGroup,
-  CCard,
-  CCardBody,
-  CCardFooter,
-  CCardHeader,
-  CCol,
-  CProgress,
-  CRow,
-  CCallout
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
 import { userContext } from '../../context/UserContext'
 import { toast, ToastContainer } from 'react-toastify'
-import MainChartExample from '../charts/MainChartExample.js'
-import Toaster from '../notifications/toaster/Toaster'
 import { url } from 'src/helpers/Helpers'
 import { Link } from 'react-router-dom'
+import Bell from '../../images/bell.svg';
+import dashboard from '../../images/dashboard.svg';
+import newImg from '../../images/new.svg';
+import tickets from '../../images/tickets.svg';
+import userImg from '../../images/user.svg';
 
-const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
-const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
 
 const Dashboard = () => {
 
@@ -56,13 +43,83 @@ const Dashboard = () => {
     <>
       <ToastContainer />
       <section>
+
+        {/* header menus section */}
+        <div className="d-flex justify-content-center mx-auto head_menus">
+          <div className="image_box">
+            <img src={dashboard} />
+          </div>
+          <div className="image_box">
+            <img src={tickets} />
+          </div>
+          <div className="image_box">
+            <img src={newImg} />
+          </div>
+          <div className="image_box">
+            <img src={Bell} />
+          </div>
+          <div className="image_box">
+            <img src={userImg} />
+          </div>
+        </div>
+
+        {/* welcome User name */}
+        <div className="user_welcome">
+          <h1>Welcome back {user?.userData.fname} !</h1>
+        </div>
+
+        <div className="container">
+
+          {/* ticket cards */}
+          <div className="all_ticket_cards justify-content-between row">
+            <div className="col-md-3 py-3 ticket_card">
+              <h1>2</h1>
+              <h4>open tickets</h4>
+            </div>
+            <div className="col-md-3 py-3 ticket_card">
+              <h1>2</h1>
+              <h4>in progress tickets</h4>
+            </div>
+            <div className="col-md-3 py-3 ticket_card">
+              <h1>2</h1>
+              <h4>closed tickets</h4>
+            </div>
+          </div>
+
+          {/* tickets review cards  */}
+          <div className="ticket_review_cards row">
+            <div className="col-md-6 last_open_ticket_card">
+              <div className='row pb-2'>
+                <h5 className='col-sm-8'> <i className="fa fa-folder-open"></i> Last Open Tickets</h5>
+                <div className='col-sm-4 d-flex justify-content-sm-end view_all_btn'>
+                  <a href="#">view all</a>
+                </div>
+              </div>
+              <div className='pt-2'>
+                <div className='row '>
+                  <p className='col-sm-8'>add test</p>
+                  <div className='col-sm-4 d-flex justify-content-sm-end opened_btn'>
+                  <a href="">Opened</a>
+                  </div>
+                </div>
+                <div className='row last_open_ticket_card_ticket_id'>
+                  <p><i className='fa fa-info-circle'></i>Ticket ID : 50</p>
+                  <p><i className='fa fa-clock-o'></i> 51 minutes ago</p>
+                  <p><i className='fa fa-archive'></i> Product : Payments</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
         <div>
-          <h4>Last Open Tickets</h4>
+          <h4> <i class="fa fa-folder-open"></i> Last Open Tickets</h4>
           <ul>
             {openTickes?.map(item => {
               return (
                 <li>
-                  <Link  to={`/ticket/${item.id}/`} >
+                  <Link to={`/ticket/${item.id}/`} >
                     <div>
                       <p>{item.subject}</p>
                       <p>Ticket-id : {item.id} <span>time :- {item.created_at}</span></p>
